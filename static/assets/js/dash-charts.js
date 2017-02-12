30a087a11d3d9b201c5b4e9e2732149b970533a3
jeremy.hon <jeremy.hon.gy@gmail.com>
2017-02-12 01:46:34 -0800

    minor fixes

--- static/assets/js/dash-charts.js
+++ static/assets/js/dash-charts.js
@@ -1,88 +1,51 @@
-﻿/*** First Chart in Dashboard page ***/
+var chart = function(chartName, data) {
+	return new Highcharts.Chart({
+		chart: {
+			renderTo: chartName,
+			margin: [0, 0, 0, 0],
+			backgroundColor: null,
+			plotBackgroundColor: 'none',
+		},
 
-	$(document).ready(function() {
-		info = new Highcharts.Chart({
-			chart: {
-				renderTo: 'load',
-				margin: [0, 0, 0, 0],
-				backgroundColor: null,
-                plotBackgroundColor: 'none',
-							
-			},
-			
-			title: {
-				text: null
-			},
+		title: {
+			text: null
+		},
 
-			tooltip: {
-				formatter: function() { 
-					return this.point.name +': '+ this.y +' %';
-						
-				} 	
-			},
-		    series: [
-				{
-				borderWidth: 2,
-				borderColor: '#F1F3EB',
-				shadow: false,	
-				type: 'pie',
-				name: 'Income',
-				innerSize: '65%',
-				data: [
-					{ name: 'load percentage', y: 45.0, color: '#b2c831' },
-					{ name: 'rest', y: 55.0, color: '#3d3d3d' }
-				],
-				dataLabels: {
-					enabled: false,
-					color: '#000000',
-					connectorColor: '#000000'
-				}
-			}]
-		});
-		
+		tooltip: {
+			formatter: function() {
+				return this.point.name +': '+ this.y +' %';
+			}
+		},
+
+		series: [{
+			borderWidth: 2,
+			borderColor: '#F1F3EB',
+			shadow: false,
+			type: 'pie',
+			name: 'Income',
+			innerSize: '65%',
+			data: data,
+			dataLabels: {
+				enabled: false,
+				color: '#000000',
+				connectorColor: '#000000'
+			}
+		}]
 	});
+}
 
-/*** second Chart in Dashboard page ***/
+var uptime = function(chartName, up) {
+	var down = 100 - up;
+	return chart(chartName, [
+		{ name: 'Uptime', y: up, color: '#b2c831' },
+		{ name: 'Downtime', y: down, color: '#3d3d3d' }
+	]);
+}
 
-	$(document).ready(function() {
-		info = new Highcharts.Chart({
-			chart: {
-				renderTo: 'space',
-				margin: [0, 0, 0, 0],
-				backgroundColor: null,
-                plotBackgroundColor: 'none',
-							
-			},
-			
-			title: {
-				text: null
-			},
-
-			tooltip: {
-				formatter: function() { 
-					return this.point.name +': '+ this.y +' %';
-						
-				} 	
-			},
-		    series: [
-				{
-				borderWidth: 2,
-				borderColor: '#F1F3EB',
-				shadow: false,	
-				type: 'pie',
-				name: 'SiteInfo',
-				innerSize: '65%',
-				data: [
-					{ name: 'Used', y: 65.0, color: '#fa1d2d' },
-					{ name: 'Rest', y: 35.0, color: '#3d3d3d' }
-				],
-				dataLabels: {
-					enabled: false,
-					color: '#000000',
-					connectorColor: '#000000'
-				}
-			}]
-		});
-		
-	});
-
+var storage = function(chartName, used) {
+	var unused = 100 - used;
+	return chart(chartName, [
+		{ name: 'Used', y: used, color: '#fa1d2d' },
+		{ name: 'Free', y: unused, color: '#3d3d3d' }
+	]);
+}
